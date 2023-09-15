826a6c0a17b85c237fe486289784ac79f54ff958
hakeemsyd <hamza.bcs18@iba-suk.edu.pk>
2023-09-15 20:02:25 +0500

    fixes

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,6 +3,5 @@ echo 'run after_install.sh: ' >> /home/ec2-user/next-codedeploy/deploy.log
 
 echo 'cd /home/ec2-user/next-codedeploy' >> /home/ec2-user/next-codedeploy/deploy.log
 cd /home/ec2-user/next-codedeploy >> /home/ec2-user/next-codedeploy/deploy.log
-
 echo 'yarn' >> /home/ec2-user/next-codedeploy/deploy.log 
 yarn >> /home/ec2-user/next-codedeploy/deploy.log
--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,4 +3,4 @@
 echo 'run application_start.sh: ' >> /home/ec2-user/next-codedeploy/deploy.log
 
 echo 'pm2 restart app' >> /home/ec2-user/next-codedeploy/deploy.log
-pm2 restart app >> /home/ec2-user/next-codedeploy/deploy.log
+pm2 reload app >> /home/ec2-user/next-codedeploy/deploy.log
